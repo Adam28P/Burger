70a158aee529431ce23477149ccdb65921372077
Adam P <adamparalovos@hotmail.com>
2018-10-10 01:40:58 -0400

    Allow users to create new burgers

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -16,26 +16,23 @@ router.get("/", function (req, res) {
     });
 });
 
-router.post("/api/burgers", function (req, res) {
+router.post("/burgers/create", function (req, res) {
     burger.insertOne([
         "burger_name", "devoured"
     ], [
-        req.body.burger_name, req.body.devoured
+        req.body.burger_name, '0'
     ], function (result) {
-        // Send back the ID of the new quote
-        res.json({
-            id: result.insertId
-        });
+        res.redirect("/");
     });
 });
 
-router.put("/api/burgers/:id", function (req, res) {
+router.put("/burgers/update/:id", function (req, res) {
     var condition = "id = " + req.params.id;
 
     console.log("condition", condition);
 
-    burger.update({
-        devoured: req.body.devoured
+    burger.updateOne({
+        devoured: '1'
     }, condition, function (result) {
         if (result.changedRows == 0) {
             // If no rows were changed, then the ID must not exist, so 404
